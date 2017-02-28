50a4eb1f19d530653cd60824e174de15725eb5dc
saraio <saraswathi91088@gmail.com>
2017-02-28 16:05:38 -0500

    Create SIT_connectDB.sh

---
+++ SIT_connectDB.sh
@@ -0,0 +1,21 @@
+export PATH=/oracle/11.2/11.2.0.3/bin:$PATH
+export ORACLE_HOME=/oracle/11.2/11.2.0.3
+#set linesize 9999
+sqlplus -s IDD/rx1t1dd#@(DESCRIPTION =(ADDRESS_LIST =(ADDRESS = (PROTOCOL = TCP)(HOST = LDCIDDD01)(PORT = 1810)))(CONNECT_DATA =(SERVER = DEDICATED)(SERVICE_NAME = IDDDEV))) << EOF
+set pagesize 0
+set feedback off
+set heading off 
+set echo off
+set serveroutput off
+set linesize 32767
+SET TRIMSPOOL ON
+SET TERMOUT OFFSET COLSEP ","
+set numw 20
+spool query_output.txt
+ALTER SESSION SET  NLS_DATE_FORMAT = "MM/DD/YYYY hh24:mi:ss";
+START SIT_query.sql spool off;
+exitEOFawk -F, '/,/{gsub(/ /, "", $0); print} ' query_output.txt > file1.txt
+#To sort the file
+sort -T . -t, -nk1 file1.txt > SIT_DB_data.txt
+rm file1.txt
+rm query_output.txt
